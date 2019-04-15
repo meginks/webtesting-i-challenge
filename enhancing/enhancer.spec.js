0cf0ec24c9536894678563bfc35ce84f3c46c0bf
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-04-15 17:58:53 -0500

    added jest and started writing tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,18 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+test('succeed', () => {
+
+})
+
+test('fail', () => {
+
+})
+
+test('repair', () => {
+
+})
+
+test('get', () => {
+    
+})
